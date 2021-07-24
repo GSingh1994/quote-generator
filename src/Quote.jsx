b5fe6da3f936fd2770cc52282e7800e3bc725b41
Gagandeep Singh <singh.gagandeep0034@gmail.com>
2021-07-24 02:33:37 -0500

    Display random quote on click

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 
 export function Quote() {
   const [quotes, setQuotes] = useState([]);
+  const [newQuotes, setNewQuotes] = useState();
+  const quotesRandomiser = quotes[Math.floor(Math.random() * quotes.length)]; //To select random quote from quotes array
+
   useEffect(() => {
     const fetchApi = async () => {
       let response = await fetch("https://type.fit/api/quotes");
@@ -10,11 +13,18 @@ export function Quote() {
     };
     fetchApi();
   }, []);
+
+  useEffect(() => {
+    setNewQuotes(quotesRandomiser);
+  }, [quotes]);
+
   return (
-    <div>
-      {/* {quotes.map((quote) => (
-        <h3>{quote.author}</h3>
-      ))} */}
-    </div>
+    <>
+      <h3 className="quotes-text">{newQuotes ? newQuotes.text : ""}</h3>
+      <div className="quotes-author">
+        {newQuotes ? newQuotes.author || "Anonymous" : null}
+      </div>
+      <button onClick={() => setNewQuotes(quotesRandomiser)}>New quote</button>
+    </>
   );
 }
