7eb591aabaabe6ab564fe6b06d077b276b17964c
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-06 19:51:09 -0500

    refactor to use helper functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import "./App.scss";
-import { Quote } from "./Quote";
-import { useState, useEffect } from "react";
+import './App.scss';
+import { Quote } from './Quote';
+import { useState, useEffect } from 'react';
+import { getRandColor, randomiser } from './helpers/randomColor';
 
 function App() {
   const [quotes, setQuotes] = useState([]);
-  const [newQuotes, setNewQuotes] = useState();
+  const [newQuote, setNewQuote] = useState();
   const [color, setColor] = useState();
-  const quotesRandomiser = quotes[Math.floor(Math.random() * quotes.length)]; //To select random quote from quotes array
 
   useEffect(() => {
     const fetchApi = async () => {
-      let response = await fetch("https://type.fit/api/quotes");
+      let response = await fetch('https://type.fit/api/quotes');
       const data = await response.json();
       setQuotes(data);
       setColor(getRandColor(1));
@@ -18,36 +18,18 @@ function App() {
     fetchApi();
   }, []);
 
-  const getRandColor = (brightness) => {
-    // Six levels of brightness from 0 to 5, 0 being the darkest
-    var rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
-    var mix = [brightness * 51, brightness * 51, brightness * 51]; //51 => 255/5
-    var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(
-      function (x) {
-        return Math.round(x / 2.0);
-      }
-    );
-    return "rgb(" + mixedrgb.join(",") + ")";
-  };
-
   useEffect(() => {
-    setNewQuotes(quotesRandomiser);
+    setNewQuote(randomiser(quotes));
   }, [quotes]);
 
   const handleClick = () => {
-    setNewQuotes(quotesRandomiser);
+    setNewQuote(randomiser(quotes));
     setColor(getRandColor(1));
   };
-  const [state, setState] = useState(false);
 
   return (
     <div className="App" style={{ backgroundColor: color }}>
-      <Quote
-        color={color}
-        newQuotes={newQuotes}
-        handleClick={handleClick}
-        state={state}
-      />
+      <Quote color={color} newQuote={newQuote} handleClick={handleClick} />
     </div>
   );
 }
--- src/Quote.jsx
+++ src/Quote.jsx
@@ -1,40 +1,26 @@
-import { FaQuoteLeft, FaTwitterSquare, FaFacebookSquare } from "react-icons/fa";
+import { FaQuoteLeft, FaTwitterSquare, FaFacebookSquare } from 'react-icons/fa';
 
-export function Quote({ color, newQuotes, handleClick }) {
+export function Quote({ color, newQuote, handleClick }) {
   return (
     <>
       <div id="quote-box" style={{ color: color }}>
         <h2 id="text">
-          <FaQuoteLeft style={{ marginRight: "1rem" }} />
-          {newQuotes ? newQuotes.text : ""}
+          <FaQuoteLeft style={{ marginRight: '1rem' }} />
+          {newQuote ? newQuote.text : ''}
         </h2>
 
-        <div id="author">
-          {newQuotes.author ? "- " + newQuotes.author : "- Anonymous"}
-        </div>
+        <div id="author">{newQuote.author ? '- ' + newQuote.author : '- Anonymous'}</div>
 
         <div className="user-section">
           <div className="icons">
-            <a
-              className="icon-quote"
-              title="Tweet this Quote"
-              href="https://www.twitter.com/"
-            >
-              <FaTwitterSquare size={"2.5em"} color={color} />
+            <a className="icon-quote" title="Tweet this Quote" href="https://www.twitter.com/">
+              <FaTwitterSquare size={'2.5em'} color={color} />
             </a>
-            <a
-              className="icon-quote"
-              title="share on FB"
-              href="https://www.facebook.com/"
-            >
-              <FaFacebookSquare size={"2.5em"} color={color} />
+            <a className="icon-quote" title="share on FB" href="https://www.facebook.com/">
+              <FaFacebookSquare size={'2.5em'} color={color} />
             </a>
           </div>
-          <button
-            id="new-quote"
-            onClick={handleClick}
-            style={{ backgroundColor: color }}
-          >
+          <button id="new-quote" onClick={handleClick} style={{ backgroundColor: color }}>
             New Quote
           </button>
         </div>
@@ -44,5 +30,5 @@ export function Quote({ color, newQuotes, handleClick }) {
 }
 
 Quote.defaultProps = {
-  newQuotes: "Honesty is the best policy",
+  newQuote: 'Honesty is the best policy',
 };
